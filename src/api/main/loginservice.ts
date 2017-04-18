e6d2037f63fcc1316635751d9c9872f171674e7f
Shreyas <shreyas.bande@tcs.com>
2017-04-18 15:07:34 +0900

    Adding data access methods and setting the online status of user

--- src/api/main/loginservice.ts
+++ src/api/main/loginservice.ts
@@ -6,6 +6,7 @@ import * as jwt from 'jsonwebtoken';
 class LoginService {
   private username: string;
   private password: string;
+  private userDetail: any;
 
   constructor(req: any) {
     this.username = req.username;
@@ -18,7 +19,7 @@ class LoginService {
       let userQuery = `select * from admin.user where name='${this.username}' and password='${this.password}' and is_active = true;`;
 
       db.fetch(userQuery)
-        .then((result) => {
+        .then((result: any) => {
           if (_.isEmpty(result)) {
             return reject({
               message: 'invalid credentials'
@@ -29,7 +30,34 @@ class LoginService {
           console.log('result: ', result);
           const token = jwt.sign({id: result.name}, 'super.super.secret.shhh', {expiresIn: 60});
           result['token'] = token;
-          return resolve(result);
+
+          this.userDetail = result;
+          return result;
+          // return resolve(result);
+        })
+        .then((result: any) => {
+          // update the is online status for the user.
+          return db.createTransaction();
+        })
+        .then(() => {
+          const q = {
+              key: 'updateOnlineStatusQuery',
+              value: {
+                text: 'UPDATE admin.user SET is_online = $1',
+                values: [true]
+              }
+            };
+
+            return db.executeQuery(q);
+        })
+        .then(() => {
+          console.log('status updated successfully');
+          return db.commitTransaction();
+        })
+        .then(() => {
+          console.log('update complete');
+          this.userDetail.is_online = true;
+          return resolve(this.userDetail);
         })
         .catch((err) => {
           return reject({
--- src/dataaccess/dataaccess.ts
+++ src/dataaccess/dataaccess.ts
@@ -1,12 +1,19 @@
 'use strict';
 import * as pg from 'pg';
+import * as transaction from 'pg-transaction';
 pg.defaults.poolSize = 15;
 
 class DataAccess {
   private db: any;
+  private txn: any;
+  private done: any;
+  private client: any;
 
   constructor() {
-    this.db = 'postgres://postgres:12345@localhost:5432/letstalk';
+    this.db     = 'postgres://postgres:12345@localhost:5432/letstalk';
+    this.txn    = null;
+    this.done   = null;
+    this.client = null;
   }
 
   public fetch(query) {
@@ -26,6 +33,86 @@ class DataAccess {
           return resolve(result.rows);
         });
       });
+    });
+  }
+
+  public createTransaction() {
+    const base = this;
+    return new Promise((resolve, reject) => {
+      pg.connect(base.db, (err, client, done) => {
+        if (err) {
+          done();
+          console.log('Error occurred in creating transaction');
+          return reject(err);
+        }
+
+        const txn = new transaction(client);
+        txn.begin();
+
+        base.txn    = txn;
+        base.done   = done;
+        base.client = client;
+
+        return resolve(true);
+      });
+    });
+  }
+
+  public executeQuery(query) {
+    const base = this;
+    return new Promise((resolve, reject) => {
+      console.log('newQuery is '  + JSON.stringify(query));
+      base.txn.query(query.value.text, query.value.values, (err, result) => {
+        if (err) {
+          base.txn.rollback();
+          console.log(`P EQP. Error while creating database connection ${err}`);
+          return reject(err);
+        }
+
+        console.log(`P EQP. Query ${query.key} executed successfully. >> ${query.value}`);
+        return resolve(0);
+      });
+    });
+  }
+
+  public fetchInTransaction(query) {
+    const base = this;
+    return new Promise((resolve, reject) => {
+      base.client.query(query.value.text, query.value.values, (err, result) => {
+        if (err) {
+          console.log(`P FNTP. Error while fetching records for ${query.key} >> ${query.value}, ${err.message}`);
+          return reject(err);
+        }
+
+        console.log(`P FNTP. ${query.key} >> ${query.value}.`);
+        return resolve(result.rows);
+      });
+    });
+  }
+
+  public commitTransaction() {
+    const base = this;
+    return new Promise((resolve, reject) => {
+      base.txn.commit((err) => {
+        base.done();
+        if (err) {
+          base.txn.rollback();
+          console.log( `P CmTP. Error while creating database connection ${err.message}`);
+          return reject(err);
+        }
+
+        console.log(`P CmTP. Transaction committed successfully.  >> ${base.txn}`);
+        return resolve(true);
+      });
+    });
+  }
+
+  public rollbackTransaction() {
+    const base = this;
+    return new Promise((resolve, reject) => {
+      base.txn.rollback();
+      base.done();
+      return resolve(true);
     });
   }
 }
